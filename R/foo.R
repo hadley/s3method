70e6ea5fd909a742de425d7cdf55b67bed1f10e9
Hadley Wickham <h.wickham@gmail.com>
2020-04-28 07:05:12 -0500

    Initial commit

---
+++ R/foo.R
@@ -0,0 +1,36 @@
+#' Foo
+#' @name foo
+#' @keywords internal
+NULL
+
+#' @rdname foo
+#' @export
+foo1 <- function(x, y) UseMethod("foo1")
+#' @export
+foo1.more <- function(x, y, z) NULL
+#' @export
+foo1.fewer <- function(x) NULL
+
+#' @rdname foo
+#' @export
+foo2 <- function(x, y, ...) UseMethod("foo2")
+#' @export
+foo2.more <- function(x, y, z, ...) NULL
+#' @export
+foo2.fewer <- function(x, ...) NULL
+
+#' @rdname foo
+#' @export
+foo3 <- function(x, ..., y) UseMethod("foo3")
+#' @export
+foo3.more <- function(x, ..., y, z) NULL
+#' @export
+foo3.fewer <- function(x, ...) NULL
+
+#' @rdname foo
+#' @export
+foo4 <- function(..., x, y) UseMethod("foo4")
+#' @export
+foo4.more <- function(..., x, y, z) NULL
+#' @export
+foo4.fewer <- function(...) NULL
